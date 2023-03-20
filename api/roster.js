db5a83346b801e0355869d0fe20d5a5db35d8465
DynamicPunch <michaelgardner231@gmail.com>
2023-03-19 23:29:14 -0400

    i

--- api/roster.js
+++ api/roster.js
@@ -19,6 +19,6 @@ export default async function handler(request, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
-    res.json(dog-roster);
+    res.json(roster);
   }
 
